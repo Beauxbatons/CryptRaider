0c98414f91646fe240d1d509171627e9b13166c6
Beauxbatons <cetinmelike5.me@gmail.com>
2023-06-19 20:36:23 +0300

    edited mover

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -29,6 +29,17 @@ void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponent
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-		UE_LOG(LogTemp, Display, TEXT("sea"));
+		AActor* Owner = GetOwner();
+
+		FString Name = Owner->GetActorNameOrLabel();
+
+		FVector OwnerLocation = Owner->GetActorLocation();
+
+		FString OwnerLocationString = OwnerLocation.ToCompactString();
+
+
+
+		UE_LOG(LogTemp, Display, TEXT("Mover Owner: %s with location %s"), *Name, *OwnerLocationString);
+
 }
 
