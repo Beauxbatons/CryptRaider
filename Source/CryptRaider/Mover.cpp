b3d427b486904153631a32c234b84a9769b7da3c
Beauxbatons <cetinmelike5.me@gmail.com>
2023-06-20 13:08:55 +0300

    Updated mover functions

--- Source/CryptRaider/Mover.cpp
+++ Source/CryptRaider/Mover.cpp
@@ -3,6 +3,9 @@
 
 #include "Mover.h"
 
+#include "Math/UnrealMathUtility.h"
+
+
 // Sets default values for this component's properties
 UMover::UMover()
 {
@@ -19,7 +22,7 @@ void UMover::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	OriginalLocation = GetOwner()->GetActorLocation();
 	
 }
 
@@ -28,18 +31,20 @@ void UMover::BeginPlay()
 void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+	
 
-		AActor* Owner = GetOwner();
+	if (ShouldMove)
+	{
+		
+	FVector CurrentLocation = GetOwner()->GetActorLocation();
+	FVector TargetLocation = OriginalLocation + MoveOffset;
+	float Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
 
-		FString Name = Owner->GetActorNameOrLabel();
+	FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
 
-		FVector OwnerLocation = Owner->GetActorLocation();
+	GetOwner()->SetActorLocation(NewLocation);
 
-		FString OwnerLocationString = OwnerLocation.ToCompactString();
-
-
-
-		UE_LOG(LogTemp, Display, TEXT("Mover Owner: %s with location %s"), *Name, *OwnerLocationString);
-
+	}
 }
 
